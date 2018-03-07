61860c45c9d73e4a532e8c4b6ad96b6263be0f69
Lukáš Ruczkowski <lukas.ruczkowski@green-light.com>
2018-03-07 11:33:49 +0100

    Enable HTML responses for browser by default

--- tests/test.js
+++ tests/test.js
@@ -48,7 +48,6 @@ describe('tracy.js', () => {
 
     it('should display HTML error to browser request', () => {
       tracy.enable();
-      tracy.enableHtmlResponse();
       const cb = () => {throw new exceptions.LogicalException('test', 400, {code: 123}, new Error('PREVIOUS_ERROR'));};
       const tracyCb = tracy.catcher(cb);
 
